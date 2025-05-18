7bb23593ec133dca68f4a16f1405d86aaca2404b
김희진 <gimijin@gimhuijin-ui-MacBookAir.local>
2025-05-18 22:32:45 +0900

    [fix] : firebase.json 참조

--- src/main/java/com/DreamOfDuck/account/config/FirebaseConfig.java
+++ src/main/java/com/DreamOfDuck/account/config/FirebaseConfig.java
@@ -4,20 +4,27 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+//    @Value("${firebase.service-account}")
+//    private String serviceAccountBase64;
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
 
         FileInputStream serviceAccount =
-                new FileInputStream(getClass().getClassLoader().getResource("firebase.json").getFile());
+                new FileInputStream("src/main/resources/firebase.json");
 
+        //InputStream serviceAccount = new ByteArrayInputStream(Base64.decodeBase64(serviceAccountBase64));
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
@@ -26,7 +33,6 @@ public class FirebaseConfig {
 
         return app;
     }
-
 
     @Bean
     public FirebaseAuth getFirebaseAuth() throws IOException {
