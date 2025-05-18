107831b452c98bfa87f501abdea99a7ffcff80a5
Heejin Kim <inging007@naver.com>
2025-05-18 20:21:27 +0900

    [feat] : google login

---
+++ src/main/java/com/DreamOfDuck/account/config/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package com.DreamOfDuck.account.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/firebase.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+
+        return app;
+    }
+
+
+    @Bean
+    public FirebaseAuth getFirebaseAuth() throws IOException {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp());
+        return firebaseAuth;
+    }
+}
--- src/main/java/com/DreamOfDuck/account/filter/JWTFilter.java
+++ src/main/java/com/DreamOfDuck/account/filter/JWTFilter.java
@@ -1,5 +1,6 @@
-package com.DreamOfDuck.account.jwt;
+package com.DreamOfDuck.account.filter;
 
+import com.DreamOfDuck.account.jwt.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
