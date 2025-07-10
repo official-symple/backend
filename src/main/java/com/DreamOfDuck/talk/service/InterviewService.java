0bcad652b5f9531dc67af55bf5206df3b4840901
Heejin Kim <inging007@naver.com>
2025-07-11 00:25:34 +0900

    [fix] : interview 변경

--- src/main/java/com/DreamOfDuck/talk/service/InterviewService.java
+++ src/main/java/com/DreamOfDuck/talk/service/InterviewService.java
@@ -28,12 +28,20 @@ public class InterviewService {
                     .question1(interviewCreateRequest.getQuestion1())
                     .question2(interviewCreateRequest.getQuestion2())
                     .question3(interviewCreateRequest.getQuestion3())
+                    .question3_2(interviewCreateRequest.getQuestion3_2())
+                    .question3_3(interviewCreateRequest.getQuestion3_3())
                     .question4(interviewCreateRequest.getQuestion4())
                     .question5(interviewCreateRequest.getQuestion5())
+                    .question5_2(interviewCreateRequest.getQuestion5_2())
                     .question6(interviewCreateRequest.getQuestion6())
+                    .question6_2(interviewCreateRequest.getQuestion6_2())
+                    .question6_3(interviewCreateRequest.getQuestion6_3())
                     .question7(interviewCreateRequest.getQuestion7())
                     .question8(interviewCreateRequest.getQuestion8())
+                    .question8_2(interviewCreateRequest.getQuestion8_2())
                     .question9(interviewCreateRequest.getQuestion9())
+                    .question9_2(interviewCreateRequest.getQuestion9_2())
+                    .question10(interviewCreateRequest.getQuestion10())
                     .host(member)
                     .build();
 
@@ -43,12 +51,20 @@ public class InterviewService {
             interview.setQuestion1(interviewCreateRequest.getQuestion1());
             interview.setQuestion2(interviewCreateRequest.getQuestion2());
             interview.setQuestion3(interviewCreateRequest.getQuestion3());
+            interview.setQuestion3_2(interviewCreateRequest.getQuestion3_2());
+            interview.setQuestion3_3(interviewCreateRequest.getQuestion3_3());
             interview.setQuestion4(interviewCreateRequest.getQuestion4());
             interview.setQuestion5(interviewCreateRequest.getQuestion5());
+            interview.setQuestion5_2(interviewCreateRequest.getQuestion5_2());
             interview.setQuestion6(interviewCreateRequest.getQuestion6());
+            interview.setQuestion6_2(interviewCreateRequest.getQuestion6_2());
+            interview.setQuestion6_3(interviewCreateRequest.getQuestion6_3());
             interview.setQuestion7(interviewCreateRequest.getQuestion7());
             interview.setQuestion8(interviewCreateRequest.getQuestion8());
+            interview.setQuestion8_2(interviewCreateRequest.getQuestion8_2());
             interview.setQuestion9(interviewCreateRequest.getQuestion9());
+            interview.setQuestion9_2(interviewCreateRequest.getQuestion9_2());
+            interview.setQuestion10(interviewCreateRequest.getQuestion10());
             return InterviewResponse.from(interview);
 
         }
