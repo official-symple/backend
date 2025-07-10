72b11f968fa7c4afb4625998feaf161e39acf3df
Heejin Kim <inging007@naver.com>
2025-07-11 00:02:40 +0900

    [fix] : 접수면접 table 속성 변경

--- src/main/java/com/DreamOfDuck/talk/dto/request/InterviewCreateRequest.java
+++ src/main/java/com/DreamOfDuck/talk/dto/request/InterviewCreateRequest.java
@@ -6,31 +6,72 @@ import lombok.Data;
 
 @Data
 public class InterviewCreateRequest {
-    @Schema(example="접수면접 1 답변")
+    @Schema(example = "접수면접 1 답변")
     @NotNull
-    String question1;
-    @Schema(example="접수면접 2 답변")
+    private String question1;
+
+    @Schema(example = "접수면접 2 답변")
     @NotNull
-    String question2;
-    @Schema(example="접수면접 3 답변")
+    private String question2;
+
+    @Schema(example = "접수면접 3 답변")
     @NotNull
-    String question3;
-    @Schema(example="접수면접 4 답변")
+    private String question3;
+
+    @Schema(example = "접수면접 3-2 답변")
     @NotNull
-    String question4;
-    @Schema(example="접수면접 5 답변")
+    private String question3_2;
+
+    @Schema(example = "접수면접 3-3 답변")
     @NotNull
-    String question5;
-    @Schema(example="접수면접 6 답변")
+    private String question3_3;
+
+    @Schema(example = "접수면접 4 답변")
     @NotNull
-    String question6;
-    @Schema(example="접수면접 7 답변")
+    private String question4;
+
+    @Schema(example = "접수면접 5 답변")
     @NotNull
-    String question7;
-    @Schema(example="접수면접 8 답변")
+    private String question5;
+
+    @Schema(example = "접수면접 5-2 답변")
     @NotNull
-    String question8;
-    @Schema(example="접수면접 9 답변")
+    private String question5_2;
+
+    @Schema(example = "접수면접 6 답변")
     @NotNull
-    String question9;
+    private String question6;
+
+    @Schema(example = "접수면접 6-2 답변")
+    @NotNull
+    private String question6_2;
+
+    @Schema(example = "접수면접 6-3 답변")
+    @NotNull
+    private String question6_3;
+
+    @Schema(example = "접수면접 7 답변")
+    @NotNull
+    private String question7;
+
+    @Schema(example = "접수면접 8 답변")
+    @NotNull
+    private String question8;
+
+    @Schema(example = "접수면접 8-2 답변")
+    @NotNull
+    private String question8_2;
+
+    @Schema(example = "접수면접 9 답변")
+    @NotNull
+    private String question9;
+
+    @Schema(example = "접수면접 9-2 답변")
+    @NotNull
+    private String question9_2;
+
+    @Schema(example = "접수면접 10 답변")
+    @NotNull
+    private String question10;
+
 }
--- src/main/java/com/DreamOfDuck/talk/entity/Interview.java
+++ src/main/java/com/DreamOfDuck/talk/entity/Interview.java
@@ -23,12 +23,20 @@ public class Interview extends TimeStamp {
     private String question1;
     private String question2;
     private String question3;
+    private String question3_2;
+    private String question3_3;
     private String question4;
     private String question5;
+    private String question5_2;
     private String question6;
+    private String question6_2;
+    private String question6_3;
     private String question7;
     private String question8;
+    private String question8_2;
     private String question9;
+    private String question9_2;
+    private String question10;
 
     @OneToOne
     @JoinColumn(name="memberId")
