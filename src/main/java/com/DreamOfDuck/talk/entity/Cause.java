070f8acf3743444b4c523595afdb9f9ceb2b2afb
Heejin Kim <inging007@naver.com>
2025-07-10 21:43:53 +0900

    [fix] : Cause 변경

--- src/main/java/com/DreamOfDuck/talk/entity/Cause.java
+++ src/main/java/com/DreamOfDuck/talk/entity/Cause.java
@@ -9,14 +9,16 @@ import org.apache.coyote.BadRequestException;
 @Getter
 @RequiredArgsConstructor
 public enum Cause {
-    CAUSE1(0, "인간관계"),
-    CAUSE2(1, "돈"),
-    CAUSE3(2, "진로"),
-    CAUSE4(3, "학업"),
-    CAUSE5(4, "건강"),
-    CAUSE6(5, "가족"),
-    CAUSE7(6, "연인"),
-    CAUSE8(7, "사별");
+    CAUSE1(0, "가족"),
+    CAUSE2(1, "반려동물"),
+    CAUSE3(2, "건강"),
+    CAUSE4(3, "돈"),
+    CAUSE5(4, "연애"),
+    CAUSE6(5, "우정"),
+    CAUSE7(6, "직장"),
+    CAUSE8(7, "진로,취업"),
+    CAUSE9(8, "학업"),
+    CAUSE10(9, "기타");
 
     private final Integer id;
     private final String text;
