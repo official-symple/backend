8e02805ec9368a5855d34a6d884b815856be2d2a
Heejin Kim <inging007@naver.com>
2025-07-15 17:18:25 +0900

    [feat] : CORS 설정 추가

---
+++ src/main/java/com/DreamOfDuck/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.DreamOfDuck.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private static final String CORS_URL_PATTERN = "/**";
+    private static final String CORS_METHOD = "*";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping(CORS_URL_PATTERN)
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization");
+    }
+}
