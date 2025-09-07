aa5f3cea65872f743ef92bda309e80a155187fb1
김희진 <inging007@naver.com>
2025-09-08 00:32:04 +0900

    [fix] : 커스텀 에러 200 응답 코드 변경

--- src/main/java/com/DreamOfDuck/global/exception/CustomException.java
+++ src/main/java/com/DreamOfDuck/global/exception/CustomException.java
@@ -1,8 +1,11 @@
 package com.DreamOfDuck.global.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CustomException extends RuntimeException {
     private final ErrorCode errorCode;
 
--- src/main/java/com/DreamOfDuck/global/exception/ExceptionAdvice.java
+++ src/main/java/com/DreamOfDuck/global/exception/ExceptionAdvice.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import com.DreamOfDuck.global.dto.response.ErrorResponse;
 import com.DreamOfDuck.global.dto.response.result.ExceptionResult;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -37,8 +38,16 @@ public class ExceptionAdvice {
 
     //커스텀 예외
     @ExceptionHandler(CustomException.class)
-    public ErrorResponse<?> handleCustomException(CustomException e) {
-        return ErrorResponse.of(e.getErrorCode().getErrorCode(), e.getErrorCode().getMessage());
+    public ResponseEntity<ErrorResponse<?>> handleCustomException(CustomException e) {
+        ErrorResponse<?> body = ErrorResponse.of(
+                e.getErrorCode().getErrorCode(),
+                e.getErrorCode().getMessage()
+        );
+
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpCode()) // enum의 httpCode 사용
+                .body(body);
+
     }
 
     //파라미터 검증 예외
