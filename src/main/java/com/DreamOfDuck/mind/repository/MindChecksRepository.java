66321c21ff1852f24b099a19e4365bc2113df14f
김희진 <inging007@naver.com>
2025-09-11 19:15:59 +0900

    [fix] : lazy loading 문제 해결

--- src/main/java/com/DreamOfDuck/mind/repository/MindChecksRepository.java
+++ src/main/java/com/DreamOfDuck/mind/repository/MindChecksRepository.java
@@ -3,7 +3,9 @@ package com.DreamOfDuck.mind.repository;
 import com.DreamOfDuck.account.entity.Member;
 import com.DreamOfDuck.mind.entity.MindCheck;
 import com.DreamOfDuck.mind.entity.MindChecks;
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,8 +13,23 @@ import java.util.List;
 
 @Repository
 public interface MindChecksRepository extends JpaRepository<MindChecks, Long> {
+    @Query("SELECT mc FROM MindChecks mc " +
+            "LEFT JOIN FETCH mc.dayMindCheck " +
+            "LEFT JOIN FETCH mc.nightMindCheck " +
+            "WHERE mc.host = :host AND mc.date BETWEEN :start AND :end " +
+            "ORDER BY mc.date DESC")
     List<MindChecks> findByHostAndDateBetweenOrderByDateDesc(
-            Member host, LocalDate start, LocalDate end
+            @Param("host") Member host,
+            @Param("start") LocalDate start,
+            @Param("end") LocalDate end
     );
-    List<MindChecks> findByHostAndDate(Member host, LocalDate date);
+
+    @Query("SELECT mc FROM MindChecks mc " +
+            "LEFT JOIN FETCH mc.dayMindCheck " +
+            "LEFT JOIN FETCH mc.nightMindCheck " +
+            "WHERE mc.host = :host AND mc.date = :date")
+    List<MindChecks> findByHostAndDate(
+            @Param("host") Member host,
+            @Param("date") LocalDate date
+    );
 }
